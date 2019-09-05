8999cd19271c45f168030b8c147974c793ce6c22
Doug Friedel <friedel@illinois.edu>
2019-09-05 15:49:50 -0500

    added initial test

--- tests/test_archiveTools.py
+++ tests/test_archiveTools.py
@@ -1,2 +1,13 @@
 #!/usr/bin/env python2
-print "hello"
+
+import unittest
+from archivetools import backup_util as bu
+
+
+class TestBadPermissions(unittest.TestCase):
+    def test_blank(self):
+        self.assertEqual(bu.get_subdir('RAW_FILE'), 'DTS')
+        self.assertEqual(bu.get_subdir('DB_BACKUPSTUFF'), 'DB')
+        self.assertEqual(bu.get_subdir('ANYTHING_ELSE'), 'OPS')
+if __name__ == '__main__':
+    unittest.main()
